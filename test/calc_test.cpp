93b6b00af224b44189d118dd8272ac07dec33079
Ryan Troxler <rtroxler@rednovalabs.com>
2019-09-07 18:56:40 -0500

    test refactoring

--- test/calc_test.cpp
+++ test/calc_test.cpp
@@ -8,6 +8,12 @@ namespace tt = boost::test_tools;
 
 using namespace std;
 
+void assert_equal(double a, double b, double tolerance = 0.0001)
+{
+    BOOST_TEST(a == b, tt::tolerance(tolerance));
+}
+
+
 /// zero angle tests
 BOOST_AUTO_TEST_CASE(zero_angle_G7_2650_200, * utf::tolerance(0.0001))
 {
@@ -20,7 +26,7 @@ BOOST_AUTO_TEST_CASE(zero_angle_G7_2650_200, * utf::tolerance(0.0001))
 	double zero_angle = calculate_zero_angle(drag_function, ballistic_coefficient, velocity, sight_height, zero_range, 0);
 
 	cout << "zero angle: " << fixed << zero_angle << endl;
-    BOOST_TEST(zero_angle == 0.095596, tt::tolerance(0.0001));
+    assert_equal(zero_angle, 0.095596);
 }
 
 BOOST_AUTO_TEST_CASE(zero_angle_G7_2650_100, * utf::tolerance(0.0001))
@@ -34,7 +40,7 @@ BOOST_AUTO_TEST_CASE(zero_angle_G7_2650_100, * utf::tolerance(0.0001))
 	double zero_angle = calculate_zero_angle(drag_function, ballistic_coefficient, velocity, sight_height, zero_range, 0);
 
 	cout << "zero angle: " << fixed << zero_angle << endl;
-    BOOST_TEST(zero_angle == 0.065903, tt::tolerance(0.0001));
+    assert_equal(zero_angle, 0.065903);
 }
 
 BOOST_AUTO_TEST_CASE(zero_angle_G7_2800_100_0365, * utf::tolerance(0.0001))
@@ -48,7 +54,7 @@ BOOST_AUTO_TEST_CASE(zero_angle_G7_2800_100_0365, * utf::tolerance(0.0001))
 	double zero_angle = calculate_zero_angle(drag_function, ballistic_coefficient, velocity, sight_height, zero_range, 0);
 
 	cout << "zero angle: " << fixed << zero_angle << endl;
-    BOOST_TEST(zero_angle == 0.061844, tt::tolerance(0.0001));
+    assert_equal(zero_angle, 0.061844);
 }
 
 /// apply drag
@@ -61,7 +67,7 @@ BOOST_AUTO_TEST_CASE(apply_drag_g7_365, * utf::tolerance(0.0001))
 	double velocity_delta = apply_drag(drag_function, ballistic_coefficient, velocity);
 
 	cout << "velocity_delta: " << fixed << velocity_delta << endl;
-	BOOST_TEST(velocity_delta == 1173.0769, tt::tolerance(0.0001));
+	assert_equal(velocity_delta, 1173.0769);
 }
 
 BOOST_AUTO_TEST_CASE(apply_drag_g7_465, * utf::tolerance(0.0001))
@@ -73,7 +79,7 @@ BOOST_AUTO_TEST_CASE(apply_drag_g7_465, * utf::tolerance(0.0001))
 	double velocity_delta = apply_drag(drag_function, ballistic_coefficient, velocity);
 
 	cout << "velocity_delta: " << fixed << velocity_delta << endl;
-	BOOST_TEST(velocity_delta == 920.8023, tt::tolerance(0.0001));
+	assert_equal(velocity_delta, 920.8023);
 }
 
 BOOST_AUTO_TEST_CASE(apply_drag_g7_565, * utf::tolerance(0.0001))
@@ -85,7 +91,7 @@ BOOST_AUTO_TEST_CASE(apply_drag_g7_565, * utf::tolerance(0.0001))
 	double velocity_delta = apply_drag(drag_function, ballistic_coefficient, velocity);
 
 	cout << "velocity_delta: " << fixed << velocity_delta << endl;
-	BOOST_TEST(velocity_delta == 757.8284, tt::tolerance(0.0001));
+	assert_equal(velocity_delta, 757.8284);
 }
 
 BOOST_AUTO_TEST_CASE(apply_drag_g7_365_2900, * utf::tolerance(0.0001))
@@ -97,7 +103,7 @@ BOOST_AUTO_TEST_CASE(apply_drag_g7_365_2900, * utf::tolerance(0.0001))
 	double velocity_delta = apply_drag(drag_function, ballistic_coefficient, velocity);
 
 	cout << "velocity_delta: " << fixed << velocity_delta << endl;
-	BOOST_TEST(velocity_delta == 1237.6476, tt::tolerance(0.0001));
+	assert_equal(velocity_delta, 1237.6476, 0.001);
 }
 
 BOOST_AUTO_TEST_CASE(apply_drag_g7_565_3000, * utf::tolerance(0.0001))
@@ -109,7 +115,7 @@ BOOST_AUTO_TEST_CASE(apply_drag_g7_565_3000, * utf::tolerance(0.0001))
 	double velocity_delta = apply_drag(drag_function, ballistic_coefficient, velocity);
 
 	cout << "velocity_delta: " << fixed << velocity_delta << endl;
-	BOOST_TEST(velocity_delta == 842.0210, tt::tolerance(0.0001));
+	assert_equal(velocity_delta, 842.0210);
 }
 
 /// calculate range table
@@ -131,11 +137,11 @@ BOOST_AUTO_TEST_CASE(range_table_365_2650, * utf::tolerance(0.0001))
 	cout << "moa adj at 300: " << range_table[300].get_moa_correction() << endl;
 	cout << "moa adj at 500: " << range_table[500].get_moa_correction() << endl;
 	cout << "moa adj at 1000: " << range_table[1000].get_moa_correction() << endl;
-	BOOST_TEST(range_table[100].get_moa_correction() == 0.00, tt::tolerance(0.001));
-	BOOST_TEST(range_table[200].get_moa_correction() == 1.829, tt::tolerance(0.001));
-	BOOST_TEST(range_table[300].get_moa_correction() == 4.326, tt::tolerance(0.001));
-	BOOST_TEST(range_table[500].get_moa_correction() == 10.14, tt::tolerance(0.001));
-	BOOST_TEST(range_table[1000].get_moa_correction() == 28.435, tt::tolerance(0.001));
+	assert_equal(range_table[100].get_moa_correction(), 0.00, 0.001);
+	assert_equal(range_table[200].get_moa_correction(), 1.829, 0.001);
+	assert_equal(range_table[300].get_moa_correction(), 4.326, 0.001);
+	assert_equal(range_table[500].get_moa_correction(), 10.14);
+	assert_equal(range_table[1000].get_moa_correction(), 28.435);
 }
 
 BOOST_AUTO_TEST_CASE(range_table_465_2650, * utf::tolerance(0.0001))
@@ -156,11 +162,11 @@ BOOST_AUTO_TEST_CASE(range_table_465_2650, * utf::tolerance(0.0001))
 	cout << "moa adj at 300: " << range_table[300].get_moa_correction() << endl;
 	cout << "moa adj at 500: " << range_table[500].get_moa_correction() << endl;
 	cout << "moa adj at 1000: " << range_table[1000].get_moa_correction() << endl;
-	BOOST_TEST(range_table[100].get_moa_correction() == 0.00, tt::tolerance(0.01));
-	BOOST_TEST(range_table[200].get_moa_correction() == 1.775, tt::tolerance(0.001));
-	BOOST_TEST(range_table[300].get_moa_correction() == 4.185, tt::tolerance(0.001));
-	BOOST_TEST(range_table[500].get_moa_correction() == 9.699, tt::tolerance(0.001));
-	BOOST_TEST(range_table[1000].get_moa_correction() == 26.355, tt::tolerance(0.001));
+	assert_equal(range_table[100].get_moa_correction(), 0.00, 0.01);
+	assert_equal(range_table[200].get_moa_correction(), 1.7756, 0.001);
+	assert_equal(range_table[300].get_moa_correction(), 4.1853, 0.001);
+	assert_equal(range_table[500].get_moa_correction(), 9.6994);
+	assert_equal(range_table[1000].get_moa_correction(), 26.3557);
 }
 
 BOOST_AUTO_TEST_CASE(range_table_365_2900, * utf::tolerance(0.0001))
@@ -181,9 +187,9 @@ BOOST_AUTO_TEST_CASE(range_table_365_2900, * utf::tolerance(0.0001))
 	cout << "moa adj at 300: " << range_table[300].get_moa_correction() << endl;
 	cout << "moa adj at 500: " << range_table[500].get_moa_correction() << endl;
 	cout << "moa adj at 1000: " << range_table[1000].get_moa_correction() << endl;
-	BOOST_TEST(range_table[100].get_moa_correction() == 0.00, tt::tolerance(0.01));
-	BOOST_TEST(range_table[200].get_moa_correction() == 1.385, tt::tolerance(0.001));
-	BOOST_TEST(range_table[300].get_moa_correction() == 3.414, tt::tolerance(0.001));
-	BOOST_TEST(range_table[500].get_moa_correction() == 8.185, tt::tolerance(0.001));
-	BOOST_TEST(range_table[1000].get_moa_correction() == 23.164, tt::tolerance(0.001));
+	assert_equal(range_table[100].get_moa_correction(), 0.00, 0.01);
+	assert_equal(range_table[200].get_moa_correction(), 1.385, 0.001);
+	assert_equal(range_table[300].get_moa_correction(), 3.414, 0.001);
+	assert_equal(range_table[500].get_moa_correction(), 8.185, 0.001);
+	assert_equal(range_table[1000].get_moa_correction(), 23.1641, 0.001);
 }
